231f7d133484865daeee81c9bd8f965eff4b84d2
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-10-31 13:50:01 +0100

    Show more or less projects on angles icon click

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // ---
-const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
-const smallMenu = document.querySelector('.header__sm-menu')
-const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont');
+const smallMenu = document.querySelector('.header__sm-menu');
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu');
 const headerHamMenuCloseBtn = document.querySelector(
   '.header__main-ham-menu-close'
-)
-const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+);
+const optionalProjects = [...document.querySelectorAll('.project-d-none')];
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link');
+
+optionalProjects.forEach(project => project.style.display = 'none');
 
 hamMenuBtn.addEventListener('click', () => {
   if (smallMenu.classList.contains('header__sm-menu--active')) {
@@ -36,3 +39,37 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+
+// Show more or less projects
+// const showMore = document.getElementById('angles-down');
+// const showLess = document.getElementById('angles-up');
+
+// for (let i = 0; i < optionalProjects.length; i++) {
+//     if (optionalProjects[i].classList.contains('project-d-none')) {
+//         moreLessContainer.innerHTML = '<i id="angles-down" class="fa-solid fa-angles-down"></i>';
+//     } else {
+//         moreLessContainer.innerHTML = '<i id="angles-up" class="fa-solid fa-angles-up"></i>';
+//     }
+// }
+
+document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('show')) {
+        const moreLessContainer = document.getElementById('more-less');
+
+        if (optionalProjects[0].style.display === 'none') {
+            optionalProjects.forEach(project => project.style.display = 'block');
+
+            moreLessContainer.innerHTML = '<i id="angles-up" class="fa-solid fa-angles-up show"></i>';
+        } else {
+            optionalProjects.forEach(project => project.style.display = 'none');
+
+            moreLessContainer.innerHTML = '<i id="angles-up" class="fa-solid fa-angles-down show"></i>';
+
+            
+        }
+    } 
+    // else if (e.target.classList.contains('show-less')) {
+
+    // }
+})
+
